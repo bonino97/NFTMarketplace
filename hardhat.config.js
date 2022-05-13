bd2717e28ee7886dd6e46bbbc44307c6abd067a4
bonino97 <juanbonino97@gmail.com>
2022-05-12 21:30:58 -0300

    Finishing Localhost Marketplace Version.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
-const fs = require("fs");
-const privateKey = fs.readFileSync(".secret").toString();
-const projectId = "db84b3850e1b4e91a4b151ff045f6a19";
+require("dotenv").config();
+
+const { PROJECT_ID, PRIVATE_KEY } = process.env;
+
+const privateKey = PRIVATE_KEY;
+const projectId = PROJECT_ID;
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    // mumbai: {
-    //   url: `https://mainnet.infura.io/v3/${projectId}`,
-    //   accounts: [
-    //     "d774cb93d72eea481f2c2fccbf8d840bc23465677c451bb975a4d9c14b541a7e",
-    //   ],
-    // },
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${projectId}`,
-    //   accounts: [
-    //     "d774cb93d72eea481f2c2fccbf8d840bc23465677c451bb975a4d9c14b541a7e",
-    //   ],
-    // },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
   },
   solidity: {
     version: "0.8.4",
